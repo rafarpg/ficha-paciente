f43fb910d437eabaef9aecb26eb5927058ec20d8
KayqueMBM <kayque_moreira@estudante.sesisenai.org.br>
2023-11-16 11:27:25 -0300

    dhbhd

--- js/pro_efe2.js
+++ js/pro_efe2.js
@@ -1,7 +1,16 @@
 const menu = document.querySelector('.menu');
 const NavMenu = document.querySelector('.nav-menu');
-
+var c=0
 menu.addEventListener('click', () => {
+    c=c+1
+    if(c==1)
+    {
+        document.getElementById("nav-menu").style.display="flex"
+    }else{
+        c=0
+        document.getElementById("nav-menu").style.display="none"
+    }
+    
     menu.classList.toggle('ativo');
     NavMenu.classList.toggle('ativo');
 })

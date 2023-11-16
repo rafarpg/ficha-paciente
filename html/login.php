d64ce5c39a19c458091f39c995d084778a62e17c
rafarpg <rafaelpassosgol@gmail.com>
2023-11-16 11:30:30 -0300

    aaaaaaaa

--- html/login.php
+++ html/login.php
@@ -1,41 +1,31 @@
 <?php
+include("conecta.php");
     session_start();
-    // print_r($_REQUEST);
     if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']))
     {
-        // Acessa
-        include_once('conecta.php');
         $email = $_POST['email'];
         $senha = $_POST['senha'];
-        $criptografada = md5($senha);
 
-        // print_r('Email: ' . $email);
-        // print_r('<br>');
-        // print_r('Senha: ' . $senha);
-
-        $sql = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$criptografada'";
+        $sql = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'";
 
         $result = $conexao->query($sql);
-
-        // print_r($sql);
-        // print_r($result);
 
         if(mysqli_num_rows($result) < 1)
         {
             unset($_SESSION['email']);
             unset($_SESSION['senha']);
-            header('Location: tela_principal.html');
+            header('Location: login.html');
+            
         }
         else
         {
             $_SESSION['email'] = $email;
-            $_SESSION['senha'] = $criptografada;
+            $_SESSION['senha'] = $senha;
             header('Location: tela_principal.html');
         }
     }
     else
     {
-        // Não acessa
-        header('Location: login.html');
+        header('Location: tela_inicial.html');
     }
 ?>
